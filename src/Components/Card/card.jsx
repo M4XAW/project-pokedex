c730370fb92b6a272a2e3496db99fe48d9c0d7a1
M4XAW <m.picault@ecole-ipssi.net>
2023-11-02 15:23:33 +0100

    zaeaze

--- src/Components/Card/card.jsx
+++ src/Components/Card/card.jsx
@@ -4,9 +4,11 @@ import "./card.scss";
 export default function Card(props) {
   const addToLocalStorage = (pokemonData) => {
     const storedData = JSON.parse(localStorage.getItem("myPokemonList")) || [];
-    
-    const isPokemonAlreadyAdded = storedData.some(pokemon => pokemon.id === pokemonData.id);
-    
+
+    const isPokemonAlreadyAdded = storedData.some(
+      (pokemon) => pokemon.id === pokemonData.id
+    );
+
     if (!isPokemonAlreadyAdded) {
       storedData.push(pokemonData);
       localStorage.setItem("myPokemonList", JSON.stringify(storedData));
@@ -18,22 +20,24 @@ export default function Card(props) {
 
   return (
     <div key={props.index} className="card">
-      <div className="cardImage">
+      <div>
         <img
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`}
           alt={props.name}
         />
+        <div className="cardContent">
+          <p>N° {props.id}</p>
+          <h2>{props.name}</h2>
+          <p>{props.height}</p>
+          <p>{props.weight}</p>
+          <p>{props.type1}</p>
+          {props.type2 && <p>{props.type2}</p>}
+          {props.type3 && <p>{props.type3}</p>}
+        </div>
       </div>
-      <div className="cardContent">
-        <p>N° {props.id}</p>
-        <h2>{props.name}</h2>
-        <p>{props.height}</p>
-        <p>{props.weight}</p>
-        <p>{props.type1}</p>
-        {props.type2 && <p>{props.type2}</p>}
-        {props.type3 && <p>{props.type3}</p>}
-      </div>
-      <button onClick={() => addToLocalStorage(props)}>Ajouter</button>
+      <button className="addButton" onClick={() => addToLocalStorage(props)}>
+        Ajouter
+      </button>
     </div>
   );
 }
