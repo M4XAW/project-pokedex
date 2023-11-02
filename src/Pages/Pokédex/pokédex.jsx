67c29697528a6f42614d58c3ec3729dfe4c8f4f4
lenzees <tcely2003@gmail.com>
2023-11-02 15:21:55 +0100

    visuel localstorage

--- src/Pages/Pokédex/pokédex.jsx
+++ src/Pages/Pokédex/pokédex.jsx
@@ -1,24 +1,38 @@
-import { useEffect, useState } from "react";
-import "./pokédex.scss";
-
-import Card from "../../Components/Card/card";
+import React, { useEffect, useState } from "react";
+import Card from '../../Components/Card/card'
 
 export default function Pokédex() {
   const [pokemonList, setPokemonList] = useState([]);
 
   useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem("myPokemonList")) || [];
+    const storedData = Object.values(localStorage)
+      .map((item) => {
+        try {
+          return JSON.parse(item);
+        } catch (error) {
+          console.error("Erreur de parsing JSON : ", error);
+          return null;
+        }
+      })
+      .filter((item) => item !== null);
     setPokemonList(storedData);
   }, []);
+  
 
   return (
     <div>
       <h1>Liste de mes Pokémon</h1>
-      <ul>
-        {pokemonList.map((pokemon, index) => (
-          <li key={index}>{pokemon.name}{pokemon.height}</li>
-        ))}
-      </ul>
+      <div className="cards">
+      {pokemonList.map((pokemon, index) => (
+        <Card
+          key={index}
+          id={pokemon.id}
+          name={pokemon.name}
+          height={pokemon.height}
+          weight={pokemon.weight}
+        />
+      ))}
+      </div>
     </div>
   );
 }
