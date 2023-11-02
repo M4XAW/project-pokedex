332d0f06bea11b776c9aba5fcfa0200451c76378
M4XAW <m.picault@ecole-ipssi.net>
2023-11-02 12:38:11 +0100

    esteré

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -3,24 +3,44 @@ import { useEffect, useState } from "react";
 import "./home.scss";
 
 export default function Home() {
-  const [pokemon, setPokemon] = useState([]);
+ const [pokemon, setPokemon] = useState([]);
 
-  useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/10", {
+ useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151", {
       method: "GET",
     })
       .then((response) => response.json())
-      .then((data) => setPokemon(data));
-  }, []);
+      .then((data) => {
+        const promises = data.results.map((pokemon) => fetch(pokemon.url));
+        Promise.all(promises).then((responses) =>
+          Promise.all(responses.map((response) => response.json()))
+        )
+          .then((pokemonData) => setPokemon(pokemonData))
+          .catch((error) => console.error("Error:", error));
+      });
+ }, []);
 
-  return (
-    <div>
-        <h1>Home</h1>
-        <h2>{pokemon.name}</h2>
-        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-        <img src={pokemon.sprites.front_shiny} alt={pokemon.name} />
-        <h2>{pokemon.height}</h2>
-        <h2>{pokemon.weight}</h2>
-    </div>
-  )
+ return (
+    <main className="home">
+      <div className="search">
+        <input type="text" placeholder="Rechercher" />
+      </div>
+      {pokemon.map((pokemon, index) => (
+        <div key={index} className="card">
+          <div className="cardImage">
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+          </div>
+          <div className="cardContent">
+            <p>N° {pokemon.id}</p>
+            <h2>{pokemon.name}</h2>
+            <p>{pokemon.height}</p>
+            <p>{pokemon.weight}</p>
+            <p>{pokemon.types[0].type.name}</p>
+            {pokemon.types[1] && <p>{pokemon.types[1].type.name}</p>}
+            {pokemon.types[2] && <p>{pokemon.types[2].type.name}</p>}
+          </div>
+        </div>
+      ))}
+    </main>
+ );
 }
---
+++ src/Pages/Pokédex/pokédex.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Pokédex() {
+  const ls = localStorage;
+
+  let pokédex = [];
+
+  return (
+    <main className="pokédex">
+      <h1>Pokédex</h1>
+    </main>
+  );
+}
