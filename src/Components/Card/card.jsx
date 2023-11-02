8f8372a0bfefed2568579717eee1c21e3a862452
M4XAW <m.picault@ecole-ipssi.net>
2023-11-02 19:04:20 +0100

    fix css

--- src/Components/Card/card.jsx
+++ src/Components/Card/card.jsx
@@ -15,23 +15,24 @@ export default function Card(props) {
 
   return (
     <div key={props.index} className="card">
-      <p className="number">N° {props.id}</p>
-      <div>
+      <p className="number">{props.id}</p>
+      <div className="cardContent">
         <img
+          className="pokemonImage"
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`}
           alt={props.name}
         />
-        <div className="cardContent">
-          <h2>{props.name}</h2>
-          <p>{props.height}</p>
-          <p>{props.weight}</p>
-          <p>{props.type1}</p>
-          {props.type2 && <p>{props.type2}</p>}
-          {props.type3 && <p>{props.type3}</p>}
+        <div className="cardInfos">
+          <h2 className="pokemonName">{props.name}</h2>
+          <p className="pokemonInfos">{props.height}</p>
+          <p className="pokemonInfos">{props.weight}</p>
+          <p className="pokemonInfos">{props.type1}</p>
+          {props.type2 && <p className="pokemonInfos">{props.type2}</p>}
+          {props.type3 && <p className="pokemonInfos">{props.type3}</p>}
         </div>
       </div>
       <button className="addButton" onClick={() => addToLocalStorage(props)}>
-        Ajouter
+        Ajouter +
       </button>
     </div>
   );
