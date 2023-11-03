49f04b78921893f460023b4f898fa926febbe2c4
M4XAW <m.picault@ecole-ipssi.net>
2023-11-03 11:13:22 +0100

    ezr

--- src/Pages/Pokédex/pokédex.jsx
+++ src/Pages/Pokédex/pokédex.jsx
@@ -5,6 +5,7 @@ import Card from "../../Components/Card/card";
 import Pokeball from "../../Assets/Images/pokéball.png";
 
 import Popup from "../../Components/PopUp/popup";
+import SearchBar from "../../Components/SearchBar/searchBar";
 
 export default function Pokédex() {
   const [pokemonList, setPokemonList] = useState([]);
@@ -13,10 +14,13 @@ export default function Pokédex() {
   const [sortBy, setSortBy] = useState(null);
   const [showPopup, setShowPopup] = useState(false);
 
+  const [searchTerm, setSearchTerm] = useState("");
+
   useEffect(() => {
     const storedData = Object.values(localStorage) // get all the values from the local storage
       .map((item) => {
-        try { //
+        try {
+          //
           return JSON.parse(item); // parse the data
         } catch (error) {
           return null;
@@ -61,10 +65,15 @@ export default function Pokédex() {
     );
   });
 
+  const handleSearchChange = (event) => {
+    // Handle the search input
+    setSearchTerm(event.target.value); // set the search term
+  };
+
   return (
     <div className="pokédex">
       {/* <Popup onClose={() => setShowPopup(false)} /> */}
-      
+
       <div className="container">
         <div className="title">
           <img src={Pokeball} alt="pokeball" />
@@ -77,12 +86,39 @@ export default function Pokédex() {
           <input
             type="text"
             placeholder="Rechercher"
-          // value={"ezr"}
-          // onChange={"ezr"}
+            value={searchTerm}
+            onChange={handleSearchChange}
           />
         </div>
       </div>
-      {pokemonList.length === 0 ? (
+      {pokemonList.length === 0 && (
+        <div className="empty">
+          <p>Votre pokédex est vide.</p>
+        </div>
+      )}
+
+      {filteredPokemonList.length === 0 && searchTerm && (
+        <div className="empty">
+          <p>Aucun résultat</p>
+        </div>
+      )}
+
+      <div className="cards">
+        {filteredPokemonList.map((pokemon, index) => (
+          <Card
+            typeButton="removeButton"
+            key={index}
+            id={pokemon.id}
+            name={pokemon.name}
+            type1={pokemon.type1}
+            type2={pokemon.type2}
+            type3={pokemon.type3}
+            onClick={() => setShowPopup(true)}
+          />
+        ))}
+      </div>
+
+      {/* {pokemonList.length === 0 ? (
         <div className="empty">
           <p>Votre pokédex est vide.</p>
         </div>
@@ -102,7 +138,7 @@ export default function Pokédex() {
             // { showPopup && <Popup onClose={() => setShowPopup(false)} />
           ))}
         </div>
-      )}
+      )} */}
     </div>
   );
 }
